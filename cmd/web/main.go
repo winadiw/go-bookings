5b53071f1fe63978754323cd5656fe7a7ff294be
Winadi Wiratama <winadi.wiratama@dana.id>
2021-04-14 11:06:05 +0700

    Create helpers for log

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/winadiw/go-bookings/internal/config"
 	"github.com/winadiw/go-bookings/internal/handlers"
+	"github.com/winadiw/go-bookings/internal/helpers"
 	"github.com/winadiw/go-bookings/internal/models"
 	"github.com/winadiw/go-bookings/internal/render"
 )
@@ -18,6 +20,8 @@ const portNumber = "localhost:8080"
 
 var app config.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // main is the main application function
 func main() {
@@ -46,6 +50,12 @@ func run() error {
 	// change this to true when production
 	app.InProduction = false
 
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
 	// Adjust Session
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
@@ -67,8 +77,8 @@ func run() error {
 
 	repo := handlers.NewRepo(&app)
 	handlers.NewHandlers(repo)
-
 	render.NewTemplates(&app)
+	helpers.NewHelpers(&app)
 
 	return nil
 }
