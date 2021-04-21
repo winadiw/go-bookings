74a07e67cf68cb547b5f35b0e648d557363ae8a9
Winadi Wiratama <winadi.wiratama@dana.id>
2021-04-21 18:54:14 +0700

    Populate models for db

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,6 +1,10 @@
 package models
 
-import "github.com/winadiw/go-bookings/internal/forms"
+import (
+	"time"
+
+	"github.com/winadiw/go-bookings/internal/forms"
+)
 
 // TemplateData holds data send from handlers to templates
 type TemplateData struct {
@@ -21,4 +25,62 @@ type Reservation struct {
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	Phone     string `json:"phone"`
+}
+
+// Users is the user model
+type Users struct {
+	ID          int
+	FirstName   string
+	LastName    string
+	Email       string
+	Password    string
+	AccessLevel int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// Rooms is the room model
+type Rooms struct {
+	ID        int
+	RoomName  string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+// Restrictions is the restriction model
+type Restrictions struct {
+	ID              int
+	RestrictionName string
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+// Reservations is the reservation model
+type Reservations struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+	StartDate time.Time
+	EndDate   time.Time
+	RoomID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Room      Rooms
+}
+
+// RoomRestrictions is the room restriction model
+type RoomRestrictions struct {
+	ID            int
+	StartDate     time.Time
+	EndDate       time.Time
+	RoomID        int
+	ReservationID int
+	RestrictionID int
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+	Room          Rooms
+	Reservation   Reservations
+	Restriction   Restrictions
 }
